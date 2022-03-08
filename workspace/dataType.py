4e954304f2acb113e0cf876e372f3b1a1f74c8a2
jeongsam3659 <57051148+jeongsam3659@users.noreply.github.com>
2022-03-08 18:43:28 +0900

    python1

---
+++ workspace/dataType.py
@@ -0,0 +1,24 @@
+# 숫자형 (정수, 음수, 실수, 연산)
+print(5)
+print(-10)
+print(3.14)
+print(3+5)
+print(3*7)
+print(3*(3+1))
+
+print(" ")
+
+# 문자형
+print('작은따옴표')
+print("큰따옴표")
+print("ㅂㅂㅂㅂ")
+print("ㅋ"*3)
+
+print(" ")
+
+#boolean 참과 거짓
+print(5 > 10)
+print(5 < 10) 
+print(True)
+print(not True)
+print(not (5>10))
